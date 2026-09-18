6cb75ee61c7076ab75fdc6494a0f7da05ccdb1f9
Yogesh123-jaat <yc307328@gmail.com>
2026-09-18 09:01:48 +0530

    Array Sorted Or Not

---
+++ src/Sorting/ArraySortedOrNot.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+public class ArraySortedOrNot 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public boolean arraySortedorNot(int[] arr)
+	{
+		for(int i = 0; i < arr.length - 1; i++)
+		{
+			if(arr[i] > arr[i+1]) return false;
+		}
+		
+		return true;
+	}
+}
