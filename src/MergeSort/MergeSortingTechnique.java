76aba7ff4da8edcac354c84848176ed2accf177e
Yogesh123-jaat <yc307328@gmail.com>
2026-09-20 10:40:54 +0530

    Merge Sort Technique

---
+++ src/MergeSort/MergeSortingTechnique.java
@@ -0,0 +1,78 @@
+package MergeSort;
+
+public class MergeSortingTechnique 
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = {8,7,6,4,5,2,1,3,4,9,-5,2,-8,9};
+		
+		mergeSort(arr);  // TC : O(nlogn) SC : O(n)
+		print(arr);
+	}
+	
+	public static void print(int[] arr)
+	{
+		for(int ele : arr)
+		{
+			System.out.print(ele + " ");
+		}
+		
+		System.out.println();
+	}
+	
+	public static void mergeSort(int[] arr)
+	{
+		int n = arr.length;
+		int idx = 0;
+		
+		if(n == 1) return;
+		
+		int[] a = new int[n/2];
+		int[] b = new int[n - n/2];
+		
+		for(int i = 0; i < a.length; i++) a[i] = arr[idx++];
+		for(int i = 0; i < b.length; i++) b[i] = arr[idx++];
+		
+		mergeSort(a);
+		mergeSort(b);
+		
+		merge(a,b,arr);
+	}
+	
+	public static void merge(int[] a , int[] b , int[] c)
+	{
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		
+		while(i < a.length && j < b.length)
+		{
+			if(a[i] < b[j])
+			{
+				c[k] = a[i];
+				i++;
+			}
+			else
+			{
+				c[k] = b[j];
+				j++;
+			}
+			
+			k++;
+		}
+		
+		while(i < a.length)
+		{
+			c[k] = a[i];
+			i++;
+			k++;
+		}
+		
+		while(j < b.length)
+		{
+			c[k] = b[j];
+			j++;
+			k++;
+		}
+	}
+}
