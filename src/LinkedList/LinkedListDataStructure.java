26253f0423c301c8c1ffff8275db8189de60a0f5
Yogesh123-jaat <yc307328@gmail.com>
2026-09-22 13:50:53 +0530

    Own Linked list

---
+++ src/LinkedList/LinkedListDataStructure.java
@@ -0,0 +1,77 @@
+package LinkedList;
+
+class Linkedlist
+{
+	Node head;
+	Node tail;
+	int size;
+	
+	void addAtTail(int val)
+	{
+		Node temp = new Node(val);
+		if(head == null) head = tail = temp;
+		else
+		{
+			tail.next = temp;
+			tail = temp;
+		}
+		
+		size++;
+	}
+	
+	void display()
+	{
+		Node temp = head;
+		
+		while(temp != null)
+		{
+			System.out.print(temp.val + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	void addAtHead(int val)
+	{
+		Node temp = new Node(val);
+		
+		if(head == null) head = tail = null;
+		else
+		{
+			temp.next = head;
+			head = temp;
+		}
+		size++;
+	}
+	
+	void deleteAtHead()
+	{
+		if(head == null)
+		{
+			System.out.println("List is Empty...");
+			return;
+		}
+		head = head.next;
+		if(head == null) tail = null;
+		size--;
+	}
+}
+public class LinkedListDataStructure
+{
+	public static void main(String[] args) 
+	{
+		Linkedlist ll = new Linkedlist();
+		
+		ll.addAtTail(10);
+		ll.addAtTail(20);
+		ll.addAtTail(30);
+		ll.addAtTail(40);
+		
+		ll.display();
+		ll.addAtHead(50);
+		ll.addAtHead(60);
+		ll.display();
+		
+		System.out.println(ll.size);
+	}
+}
