e33ff53e5c09b777dd5e90731896db5f6d56b086
Yogesh123-jaat <yc307328@gmail.com>
2026-09-24 11:14:48 +0530

    Middle Of A LinkedList Slow Fast Approach GFG

---
+++ src/LinkedList/MiddleOfALinkedListSlowFastApproach.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class MiddleOfALinkedListSlowFastApproach 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static int getMiddle(Node head)
+	{
+		Node slow = head;
+		Node fast = head;
+		while(fast != null && fast.next != null)
+		{
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		return slow.val;
+	}
+}
