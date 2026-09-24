e0e123004748df423bf21aa016eafcb610352179
Yogesh123-jaat <yc307328@gmail.com>
2026-09-24 11:28:10 +0530

    Delete Middle Of A LinkedList Slow Fast Approach GFG

---
+++ src/LinkedList/DeleteMiddleOfALinkedListSlowFastApproachGFG.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class DeleteMiddleOfALinkedListSlowFastApproachGFG 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static Node deleteMid(Node head)
+	{
+		if(head == null || head.next == null) return null;
+		
+		Node slow = head;
+		Node fast = null;
+		Node prev = null;
+		
+		while(fast != null && fast.next != null)
+		{
+			prev = slow;
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		prev.next = slow.next;
+		
+		return head;
+	}
+}
